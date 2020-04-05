3e1603d893447af22b0d8a3ac51f5c06f9cc9099
Jim Stewart <jim@stewartstuff.com>
2020-04-05 06:07:35 -0500

    Initial Commit

---
+++ ElementaryViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElementaryViews
+//
+//  Created by James Stewart on 4/5/20.
+//  Copyright © 2020 James Stewart. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
